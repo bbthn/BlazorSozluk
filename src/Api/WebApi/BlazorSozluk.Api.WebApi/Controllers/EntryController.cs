da67d8c5aff237e0fb58bc1d5bbeeffb51a18e02
BATUHAN BATUMLU <batuhanbatumlu@outlook.com>
2023-07-06 08:17:55 +0300

    Add project files.

---
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Controllers/EntryController.cs
@@ -0,0 +1,66 @@
+﻿using BlazorSozluk.Api.Application.Features.Queries.GetEntries;
+using BlazorSozluk.Common.Models.Queries;
+using BlazorSozluk.Common.Models.RequestModels;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorSozluk.Api.WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+
+public class EntryController : BaseController
+{
+    private readonly IMediator mediator;
+
+    public EntryController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetEntries([FromQuery] GetEntriesQuery query)
+    {
+        var entries = await mediator.Send(query);
+
+        return Ok(entries);
+    }
+
+
+    [HttpPost]
+    [Route("CreateEntry")]
+    [Authorize]
+    public async Task<IActionResult> CreateEntry([FromBody] CreateEntryCommand command)
+    {
+        if (!command.CreatedById.HasValue)
+            command.CreatedById = UserId;
+
+        var result = await mediator.Send(command);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [Route("CreateEntryComment")]
+    [Authorize]
+    public async Task<IActionResult> CreateEntryComment([FromBody] CreateEntryCommentCommand command)
+    {
+        if (!command.CreatedById.HasValue)
+            command.CreatedById = UserId;
+
+        var result = await mediator.Send(command);
+
+        return Ok(result);
+    }
+
+
+    [HttpGet]
+    [Route("Search")]
+    public async Task<IActionResult> Search([FromQuery] SearchEntryQuery query)
+    {
+        var result = await mediator.Send(query);
+
+        return Ok(result);
+    }
+}
