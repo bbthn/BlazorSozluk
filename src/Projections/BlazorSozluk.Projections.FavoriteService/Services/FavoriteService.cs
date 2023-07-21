613c29a3f122005c92d59d44d37a30d344767c3f
BATUHAN BATUMLU <batuhanbatumlu@outlook.com>
2023-07-21 23:01:50 +0300

    FavoriteService added

---
+++ src/Projections/BlazorSozluk.Projections.FavoriteService/Services/FavoriteService.cs
@@ -0,0 +1,35 @@
+﻿using BlazorSozluk.Common.Events.Entry;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Projections.FavoriteService.Services;
+
+public class FavoriteService
+{
+    private readonly string connectionStr;
+
+    public FavoriteService(string connectionStr)
+    {
+        this.connectionStr = connectionStr;
+    }
+
+    public async Task CreateEntryFavEvent(CreateEntryFavEvent @event)
+    {
+        using var connection = new SqlConnection(connectionStr);
+
+        await connection.ExecuteAsync
+            ("INSERT INTO EntryFavorite (Id, EntryId, CreatedById, CreateDate) VALUES(@Id, @EntryId, @CreatedById, GETDATE())",
+            new
+            {
+                Id=Guid.NewGuid(),
+                EntryId = @event.EntryId,
+                CreatedById = @event.CreatedBy,
+            });
+
+    }
+}
--- src/Projections/BlazorSozluk.Projections.FavoriteService/Worker.cs
+++ src/Projections/BlazorSozluk.Projections.FavoriteService/Worker.cs
@@ -1,25 +1,33 @@
 using BlazorSozluk.Common;
 using BlazorSozluk.Common.Events.Entry;
 using BlazorSozluk.Common.Infrastructure;
+using BlazorSozluk.Projections.FavoriteService;
 
 namespace BlazorSozluk.Projections.FavoriteService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration configuration;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            this.configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+
+            var favService = new Services.FavoriteService(configuration.GetSection("ConnectionString")["SqlServer"]);
+
+
             QueueFactory.CreateBasicConsumer()
                 .EnsureExchange(SozlukConstants.FavExchangeName)
                 .EnsureQueue(SozlukConstants.CreateEntryFavQueueName,SozlukConstants.FavExchangeName)
                 .Receive<CreateEntryFavEvent>(fav =>
                 {
+                    favService.CreateEntryFavEvent(fav).GetAwaiter().GetResult();
                     _logger.LogInformation($"Received EntryId: {fav.EntryId}");
 
                 })
