cc936bb23a9dbf0cb1c40c7a488e796e5bd6bd1e
BATUHAN BATUMLU <batuhanbatumlu@outlook.com>
2023-07-24 07:33:04 +0300

    QueueFactory added userserviceprojection

---
+++ src/Projections/BlazorSozluk.Projections.UserService/Services/UserServices.cs
@@ -0,0 +1,38 @@
+﻿using BlazorSozluk.Common.Events.User;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Projections.UserService.Services;
+
+public class UserServices
+{
+    private readonly IConfiguration configuration;
+    public UserServices(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public async Task EmailChangedEvent(UserEmailChangedEvent @event)
+    {
+        using var connection = new SqlConnection(configuration.GetConnectionString("SqlServer"));
+
+        connection.ExecuteAsync
+            ("INSERT INTO emailconfirmation (Id, OldEmailAddress,NewEmailAddress,CreateDate) VALUES(@Id,@OldEmailAddress,@NewEmailAddress,GETDATE()",
+            new
+            {
+                Id= Guid.NewGuid(),
+                OldEmailAddress = @event.OldEmailAddress,
+                NewEmailAddress= @event.NewEmailAddress,
+            });
+
+
+
+
+    }
+
+}
--- src/Projections/BlazorSozluk.Projections.UserService/Worker.cs
+++ src/Projections/BlazorSozluk.Projections.UserService/Worker.cs
@@ -1,21 +1,33 @@
+using BlazorSozluk.Common;
+using BlazorSozluk.Common.Events.User;
+using BlazorSozluk.Common.Infrastructure;
+using BlazorSozluk.Projections.UserService.Services;
+
 namespace BlazorSozluk.Projections.UserService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration configuration;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            this.configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+            var userService = new UserServices(configuration);
+
+            QueueFactory.CreateBasicConsumer()
+                .EnsureExchange(SozlukConstants.UserExchangeName)
+                .EnsureQueue(SozlukConstants.UserEmailChangedQueueName, SozlukConstants.UserExchangeName)
+                .Receive<UserEmailChangedEvent>(user =>
+                {
+                    userService.EmailChangedEvent(user).GetAwaiter().GetResult();
+                    _logger.LogInformation($"Received old email:{user.OldEmailAddress}, new email:{user.NewEmailAddress}");
+                }).StartConsuming(SozlukConstants.UserEmailChangedQueueName);
         }
     }
 }
